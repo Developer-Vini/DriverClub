b03d4ddc54d1cba5555ce9a20dcf84a40dffe029
Vinicisu <desegames132@gmail.com>
2026-07-30 19:00:58 -0300

    add: files

---
+++ backend/migrations/1785447352036_create-users-table.js
@@ -0,0 +1,44 @@
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+export const shorthands = undefined;
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const up = (pgm) => {
+    pgm.createTable('users', {
+        id: 'id',
+        name: {
+            type: 'varchar(150)',
+            notNull: true,
+            unique: true,
+        },
+        password_hash: {
+            type: 'text',
+            notNull: true
+        },
+        phone: {
+            type: 'varchar(20)',
+            notNull: true,
+        },
+        role: {
+            type: 'varchar(20)',
+            notNull: true,
+        },
+        created_at: {
+            type: 'timestamp',
+            notNull: true,
+            default: pgm.func('now()')
+        },
+    })
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const down = (pgm) => {};
