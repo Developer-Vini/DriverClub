e7eb0e5e4c0b2fc77b897e08806593888e339e72
Vinicisu <desegames132@gmail.com>
2026-07-31 18:15:53 -0300

    fix: registation message error

--- backend/src/modules/auth/auth.service.js
+++ backend/src/modules/auth/auth.service.js
@@ -3,27 +3,23 @@ const pool = require('../../config/database');
 
 async function registrarUsuario({ name, email, password, phone, role }) {
 
-    try {
-        const usuarioExistente = await pool.query(
-            'SELECT id FROM users WHERE email = $1',
-            [email]
-        );
+    const usuarioExistente = await pool.query(
+        'SELECT id FROM users WHERE email = $1',
+        [email]
+    );
 
-        if (usuarioExistente.rows.length > 0) {
-            throw new Error("Email já cadastrado");
-        }
+    if (usuarioExistente.rows.length > 0) {
+        throw new Error("Email já cadastrado");
+    }
 
-        const passwordHash = await bcrypt.hash(password, 10);
+    const passwordHash = await bcrypt.hash(password, 10);
 
-        const resultado = await pool.query(
-            `INSERT INTO users (name, email, password_hash, phone, role)
+    const resultado = await pool.query(
+        `INSERT INTO users (name, email, password_hash, phone, role)
              VALUES ($1, $2, $3, $4, $5)
              RETURNING id, name, email, role`,
-            [name, email, passwordHash, phone, role]
-        );
-    } catch (err) {
-        console.error(err.message)
-    }
+        [name, email, passwordHash, phone, role]
+    );
 
     return resultado.rows[0];
 }
