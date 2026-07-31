b39abe4a332af2a8f86d4f96ef1218525e9184bb
Vinicisu <desegames132@gmail.com>
2026-07-31 19:30:03 -0300

    add: json web token and bcrypt

--- backend/src/modules/auth/auth.service.js
+++ backend/src/modules/auth/auth.service.js
@@ -1,4 +1,5 @@
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken')
 const pool = require('../../config/database');
 
 async function registrarUsuario({ name, email, password, phone, role }) {
@@ -24,4 +25,39 @@ async function registrarUsuario({ name, email, password, phone, role }) {
     return resultado.rows[0];
 }
 
-module.exports = { registrarUsuario }
+async function autenticarUsuario({ email, password }){
+    const resultado = await pool.query(
+        'SELECT id, name, email, password_hash, role FROM users WHERE email = $1',
+        [email]
+    );
+
+    const usuario = resultado.rows[0];
+
+    if(!usuario){
+        throw new Error("Email ou seenha inválidos");
+    }
+
+    const senhaCorreta = await bcrypt.compare(password, usuario.password_hash);
+
+    if(!senhaCorreta){
+        throw new Error('Email ou senha inválidos');
+    }
+
+    const token = jwt.sign(
+        {
+            id: usuario.id, 
+            role: usuario.role
+        },
+        process.env.JWT_SECRET,
+        {
+            expiresIn: '7d'
+        }
+    );
+
+    return { 
+        token,
+        usuario: { id: usuario.id, name: usuario.name, email: usuario.email, role: usuario.role, },
+    };
+}
+
+module.exports = { registrarUsuario, autenticarUsuario };
